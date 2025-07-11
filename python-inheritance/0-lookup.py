582003a6ed8ad15eae15a72a738b0e8e51b8fa40
charleswereangoye <c.angoye@alustudent.com>
2025-07-11 14:50:08 -0700

    my work

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
-#!/usr/bin/python3
-def lookup(obj):
-    return dir(obj)
+class MyClass:
+    def __init__(self):
+        self.x = 10
+    def method(self):
+        pass
+
+obj = MyClass()
+print(lookup(obj))

9213f03a92247e6d7ca4ae4da03564654ad7fa41
charleswereangoye <c.angoye@alustudent.com>
2025-07-18 19:55:47 +0200

    my work

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''A python script that can execute the contents of a file'''
+
+
 def read_file(filename=""):
+    '''Reads the data from outside file '''
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+        read_data = f.read()
+        print(read_data, end="")

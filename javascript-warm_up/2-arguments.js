38b6a90435b08ccfef9a4f93f230951a5bc168fc
charleswereangoye <c.angoye@alustudent.com>
2025-10-04 16:30:06 +0200

    my work

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let arguement = 'Best';
+const arguement = 'Best';
 
 if (arguement === '#') {
     console.log('No argument');
-}
+    }
 else if (arguement === 'Best') {
     console.log('Argument found');
-}
+    }
 else {
     console.log('Arguments found');
-}
+    }

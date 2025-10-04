5b05613083d682c71246a45863128a29f03c5f56
charleswereangoye <c.angoye@alustudent.com>
2025-10-04 19:37:31 +0200

    my work

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const uniqueArgs = [...new Set(args)]; // remove duplicates
+  uniqueArgs.sort((a, b) => b - a);       // sort descending
+  console.log(uniqueArgs[1]);
+}
---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  return a + b;
+}
+
+// Make the function visible outside this file
+exports.add = add;

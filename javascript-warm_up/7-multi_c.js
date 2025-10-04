027b46f57f6e3bcc85cc9c8722f0bb011bab4abc
charleswereangoye <c.angoye@alustudent.com>
2025-10-04 19:12:03 +0200

    my work

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const times = parseInt(process.argv[2]);
 
-if (isNaN(times) || times <= 0) {
+if (isNaN(times)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < times; i++) {

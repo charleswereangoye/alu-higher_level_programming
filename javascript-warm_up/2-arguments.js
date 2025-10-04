5d244e580d054f102a646a58278b3b5e7db7a4c2
charleswereangoye <c.angoye@alustudent.com>
2025-10-04 16:26:25 +0200

    my work

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let arguement = 'Best';
+
+if (arguement === '#') {
+    console.log('No argument');
+}
+else if (arguement === 'Best') {
+    console.log('Argument found');
+}
+else {
+    console.log('Arguments found');
+}

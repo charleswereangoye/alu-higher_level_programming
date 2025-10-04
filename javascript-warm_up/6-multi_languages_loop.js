6ab00ddc86caadc9fc808c3820e81ab1cd3b4cd5
charleswereangoye <c.angoye@alustudent.com>
2025-10-04 18:59:32 +0200

    my work

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const sentences = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+];
+
+let index = 0;
+while (index < sentences.length) {
+    console.log(sentences[index]);
+    index++;
+}
